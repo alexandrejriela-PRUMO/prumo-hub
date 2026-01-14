3631bb1901fab8221b698e39e181b156f3abb138
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-01-14 03:50:27 +0000

    File changes

--- src/pages/ESGAgro.jsx
+++ src/pages/ESGAgro.jsx
@@ -13,7 +13,7 @@ import {
   ChevronRight,
   Sprout
 } from 'lucide-react';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '../utils';
 import { Link } from 'react-router-dom';
 
 export default function ESGAgro() {

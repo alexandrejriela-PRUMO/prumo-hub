44721a26394a5147981e110d0d8cf07136ee640c
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-02-18 14:51:04 +0000

    File changes

--- src/pages/ConsultorClients.jsx
+++ src/pages/ConsultorClients.jsx
@@ -85,10 +85,7 @@ export default function ConsultorClients() {
     }));
   }, [clients, clientProperties]);
 
-  const handleAccessProperty = (property) => {
-    // Navegar para o Dashboard com a propriedade selecionada
-    navigate(createPageUrl('Home') + `?property=${property.id}`);
-  };
+
 
   return (
     <div className="max-w-7xl mx-auto space-y-6">

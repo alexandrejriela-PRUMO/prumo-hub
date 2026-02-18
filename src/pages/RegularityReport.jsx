4af8e78db103465a6f63142070adadef3b151f48
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-02-18 13:48:01 +0000

    File changes

--- src/pages/RegularityReport.jsx
+++ src/pages/RegularityReport.jsx
@@ -38,9 +38,13 @@ export default function RegularityReport() {
     loadUser();
   }, []);
 
+  const isConsultor = user?.user_type === 'consultor';
+
   const { data: properties = [] } = useQuery({
     queryKey: ['properties', user?.email],
-    queryFn: () => base44.entities.Property.filter({ owner_email: user.email }),
+    queryFn: () => isConsultor
+      ? base44.entities.Property.filter({ consultor_email: user.email })
+      : base44.entities.Property.filter({ owner_email: user.email }),
     enabled: !!user?.email
   });
 

2e9740b6a05be681eb24454cbac0a8dd8788c5ed
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-03-16 01:59:10 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -60,6 +60,7 @@ const consultorNavItems = [
       { name: 'Meus Clientes', page: 'ConsultorClients', icon: Users },
       { name: 'Gestão de Propriedades e Empreendimentos', page: 'Properties', icon: Building2 },
       { name: 'Meus Contratos', page: 'Contracts', icon: ScrollText },
+      { name: 'CRM de Clientes', page: 'ConsultorClients', icon: ClipboardList },
       { name: 'Config. de Pagamento', page: 'PaymentSettings', icon: CreditCard },
       { name: 'Minha Equipe', page: 'MyTeam', icon: Users },
     ]

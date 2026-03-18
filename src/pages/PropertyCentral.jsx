f56473891ac4455a7d0ee8ef7ddd1d438495dd4d
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-03-18 04:57:22 +0000

    File changes

--- src/pages/PropertyCentral.jsx
+++ src/pages/PropertyCentral.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '@/utils';
 import { Card } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';

10cd40bc0c47e631cde8c7b34e32dc08d1e3ac42
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-03-18 02:14:53 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -85,5 +85,17 @@ module.exports = {
   		}
   	}
   },
+  safelist: [
+    'bg-blue-100','text-blue-800','border-blue-200',
+    'bg-purple-100','text-purple-800','border-purple-200',
+    'bg-emerald-100','text-emerald-800','border-emerald-200',
+    'bg-orange-100','text-orange-800','border-orange-200',
+    'bg-yellow-100','text-yellow-800','border-yellow-200',
+    'bg-gray-100','text-gray-800','border-gray-200',
+    'bg-red-100','text-red-800','border-red-200',
+    'bg-pink-100','text-pink-800','border-pink-200',
+    'bg-slate-100','text-slate-800','border-slate-200',
+    'bg-indigo-100','text-indigo-800','border-indigo-200',
+  ],
   plugins: [require("tailwindcss-animate")],
 }

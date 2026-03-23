36827c541087fe26fbcb319916f8a6e5e21df34f
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-03-23 17:54:40 +0000

    File changes

--- src/pages/PropertyMapView.jsx
+++ src/pages/PropertyMapView.jsx
@@ -163,7 +163,7 @@ function downloadKml(content, filename) {
 }
 
 export default function PropertyMapView() {
-   const { effectiveEmail, isEquipe, userType } = useEffectiveUser();
+   const { effectiveEmail, isEquipe, userType, isLoading: loadingUser } = useEffectiveUser();
    const [user, setUser] = useState(null);
    const [selectedPropertyId, setSelectedPropertyId] = useState('');
    const [activeLayers, setActiveLayers] = useState({

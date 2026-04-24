5817b977367bfec0ef7d1597ae9aaf8c4807cf1d
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-04-24 16:34:01 +0000

    File changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,6 +5,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const reactPath = path.resolve(__dirname, 'node_modules/react');
+const reactDomPath = path.resolve(__dirname, 'node_modules/react-dom');
 
 export default defineConfig({
   plugins: [
@@ -15,6 +17,8 @@ export default defineConfig({
     dedupe: ['react', 'react-dom', 'react-router-dom', '@tanstack/react-query'],
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'react': reactPath,
+      'react-dom': reactDomPath,
     },
   },
 });

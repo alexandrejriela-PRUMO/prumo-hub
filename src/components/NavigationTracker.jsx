e2bed8058536a84466c14653acc829e8e4ae3327
Alexandre Juliani Riela <alexandrejriela@gmail.com>
2026-04-24 15:00:06 +0000

    File changes

--- src/components/NavigationTracker.jsx
+++ src/components/NavigationTracker.jsx
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useInRouterContext } from 'react-router-dom';
 
-export default function NavigationTracker() {
+function NavigationTrackerInner() {
   const location = useLocation();
-
   useEffect(() => {
     // Track page navigation
   }, [location.pathname]);
-
   return null;
+}
+
+export default function NavigationTracker() {
+  const inRouter = useInRouterContext();
+  if (!inRouter) return null;
+  return <NavigationTrackerInner />;
 }
